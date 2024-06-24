535d067f4146dda6f2a66f50746498ca30bce1d9
Jessy Hanifiah <jessyhanifiah@gmail.com>
2024-06-24 20:06:07 +0700

    feat: add navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+
+export const Navbar = () => {
+  const navlinks = [
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+  ];
+  return (
+    <div className="mx-auto flex max-w-7xl justify-between border-b p-4">
+      <div>
+        <Link href="/" className="font-bold">
+          Jeeehaan
+        </Link>
+      </div>
+      <ul className="flex gap-6">
+        {navlinks.map((navlink, index) => (
+          <li key={`navlink-${index}`}>
+            <Link href={navlink.href}>{navlink.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
